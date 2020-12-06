6e70e01c6b1729f895498fcc425458f913c22d35
Vladik <mogilevskiy499@gmail.com>
2020-12-06 16:17:41 +0300

    Add project

---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audio;
+    [SerializeField] private float _duration;
+
+    private Coroutine _smoothVolumeChange;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ThiefCheck(collision, 1f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ThiefCheck(collision, 0f);
+    }
+
+    private void ThiefCheck(Collider2D collision, float targetVolume)
+    {
+        if (collision.gameObject.TryGetComponent(out Thief thief))
+        {
+            if (_smoothVolumeChange != null)
+                StopCoroutine(_smoothVolumeChange);
+
+            _smoothVolumeChange = StartCoroutine(SmoothVolumeChange(_audio.volume, targetVolume));
+        }
+    }
+
+    private IEnumerator SmoothVolumeChange(float startVolume, float targetVolume)
+    {
+        float elapsed = 0;
+
+        while (_audio.volume != targetVolume)
+        {
+            _audio.volume = Mathf.Lerp(startVolume, targetVolume, elapsed / _duration);
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Thief.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Thief : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rigidbody2D;
+    private float _movementX;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _movementX = Input.GetAxis("Horizontal") * _speed;
+
+        _rigidbody2D.velocity = new Vector2(_movementX, _rigidbody2D.velocity.y);
+    }
+}
